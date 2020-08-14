1fb4c45f03eaa8229abbf18619c52dc95d264c48
Hai Nguyen <ndhai.hai@gmail.com>
2020-08-14 21:58:43 +0900

    no message

--- Final_Homework/src/main/java/learnjava/final_homework/controllers/AdminController.java
+++ Final_Homework/src/main/java/learnjava/final_homework/controllers/AdminController.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,46 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import learnjava.final_homework.form.StudentForm;
+import learnjava.final_homework.model.Class;
 import learnjava.final_homework.model.Student;
 import learnjava.final_homework.service.ClassService;
 import learnjava.final_homework.service.StudentService;
-import learnjava.final_homework.model.Class;
+
+class ChartItemObject {
+
+	String label = "Student Quantity";
+	String backgroundColor = "rgba(60,141,188,0.9)";
+	String borderColor = "rgba(60,141,188,0.8)";
+	String pointRadius = "false";
+	String pointColor = "#3b8bba";
+	String minBarLength = "0";
+	String barPercentage = "1";
+	String barThicknes = "50";
+	String maxBarThickness = "150";
+	String pointStrokeColor = "rgba(60,141,188,1)";
+	String pointHighlightFill = "#fff";
+	String pointHighlightStroke = "rgba(60,141,188,1)";
+	//
+	//so luong hoc sinh tuong duong voi tung diem so tu 0-10
+	//query tu DB
+	//
+	String[] data = {"1", "5", "0", "12" , "7", "5", "8", "7", "11", "4", "11"};
+
+}
+
+class ChartAreaObject {
+	String[] labels;
+	ChartItemObject[] datasets;
+
+}
 
 @Controller
 @RequestMapping("/admin")
@@ -83,24 +119,25 @@ public class AdminController {
 		Student student = studentService.findById(id);
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		String birthday = simpleDateFormat.format(student.getBirthday());
-		
+
 		StudentForm stForm = new StudentForm();
 		stForm.setFullName(student.getFullName());
 		stForm.setSex(student.getSex());
 		stForm.setBirthday(birthday);
 		stForm.setClassId(student.getClassId());
-		
+
 		List<Class> listClass = classService.findAll();
 		model.addAttribute("listClass", listClass);
-		
+
 		model.addAttribute("studentForm", stForm);
 		model.addAttribute("studentId", id);
 		return "editStudent";
 	}
-	
+
 	@RequestMapping(value = { "/editStudent" }, method = RequestMethod.POST)
 	public String saveEditStudent(Model model, //
-			@ModelAttribute("studentForm") StudentForm studentForm, @RequestParam(name = "id") String  id)  throws ParseException {
+			@ModelAttribute("studentForm") StudentForm studentForm, @RequestParam(name = "id") String id)
+			throws ParseException {
 
 		String fullName = studentForm.getFullName();
 		String sex = studentForm.getSex();
@@ -112,6 +149,37 @@ public class AdminController {
 		model.addAttribute("errorMessage", errorMessage);
 		return "redirect:/admin/studentList";
 	}
-	
+
+	@RequestMapping(value = { "/chart" }, method = RequestMethod.GET)
+	public String showChart(Model model) {
+
+		return "chart";
+	}
+
+	@RequestMapping(value = { "/load_chart" }, method = RequestMethod.GET)
+	public @ResponseBody String getChartValue(HttpServletRequest request) throws JsonProcessingException {
+		System.out.println("here");
+		System.out.println(request.getParameter("test"));
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+
+		//
+		//label diem so tu 1-10
+		//
+		String[] labels = {"0", "1", "2", "3" , "4", "5", "6", "7", "8", "9", "10"};
+
+		ChartItemObject datasetsItem = new ChartItemObject();
+		ChartItemObject[] datasets = {datasetsItem};
+
+		ChartAreaObject chartAreaObject = new ChartAreaObject();
+		chartAreaObject.labels = labels;
+		chartAreaObject.datasets = datasets;
+
+		String data = mapper.writeValueAsString(chartAreaObject);
+		System.out.println(data);
+
+		return data;
+	}
 
 }
